d1405c7d840ff52847a0089c526249dfcf46cbe1
ShaneGH <shane.connon@gmail.com>
2016-12-31 13:20:17 +0000

    test

---
+++ server.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+    
+}).listen(process.env.PORT || 8080);
